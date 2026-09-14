ed729f88996529e15503d0ee715a7bad3a8de08d
tarra <crazydaveangry@gmail.com>
2026-09-14 09:21:14 +0700

    Add lightweight Neovim options

---
+++ nvim/lua/volkanos/options.lua
@@ -0,0 +1,29 @@
+local opt = vim.opt
+
+opt.number = true
+opt.relativenumber = true
+opt.signcolumn = "yes"
+opt.cursorline = true
+opt.termguicolors = true
+
+opt.expandtab = true
+opt.shiftwidth = 4
+opt.tabstop = 4
+opt.smartindent = true
+
+opt.ignorecase = true
+opt.smartcase = true
+opt.scrolloff = 6
+opt.sidescrolloff = 6
+
+opt.splitbelow = true
+opt.splitright = true
+opt.wrap = false
+opt.mouse = "a"
+opt.clipboard = "unnamedplus"
+
+opt.updatetime = 250
+opt.timeoutlen = 400
+opt.laststatus = 3
+opt.showmode = false
+opt.fillchars = { eob = " " }
